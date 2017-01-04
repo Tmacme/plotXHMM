eafee8d65aba410815171ee0dcc05d2534e24bbb
Michael Knudsen <michaelk@clin.au.dk>
2017-01-04 13:20:59 +0100

    Created new Interval class

--- plotxhmm/plotxhmm.py
+++ plotxhmm/plotxhmm.py
@@ -5,3 +5,12 @@ class Parser:
 
     def __init__(self, normalized_zscores_file):
         self._df = pd.read_csv(normalized_zscores_file, sep='\t', index_col='Matrix')
+
+
+class Interval:
+
+    def __init__(self, chromosome, start, end):
+
+        self._chromosome = chromosome
+        self._start = start
+        self._end = end
