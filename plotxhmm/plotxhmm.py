837ca214378b4bbc10508a631978c1a9d167f40c
Michael Knudsen <michaelk@clin.au.dk>
2017-01-04 14:34:39 +0100

    Renamed index column

--- plotxhmm/plotxhmm.py
+++ plotxhmm/plotxhmm.py
@@ -5,6 +5,7 @@ class Parser:
 
     def __init__(self, normalized_zscores_file):
         self._df = pd.read_csv(normalized_zscores_file, sep='\t', index_col='Matrix')
+        self._df.index.names = ['SAMPLE']
 
     def subset(self, interval):
 
