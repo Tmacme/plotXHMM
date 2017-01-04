ccfdae2aa5c70ed64060267ce7a5e776bad052ec
Michael Knudsen <michaelk@clin.au.dk>
2017-01-04 12:30:36 +0100

    Using the Matrix column as index

--- plotxhmm/parser.py
+++ plotxhmm/parser.py
@@ -4,4 +4,4 @@ import pandas as pd
 class Parser:
 
     def __init__(self, normalized_zscores_file):
-        self._df = pd.read_csv(normalized_zscores_file, sep='\t')
+        self._df = pd.read_csv(normalized_zscores_file, sep='\t', index_col='Matrix')
