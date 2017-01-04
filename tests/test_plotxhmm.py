409da7942959ae638bd044d9925ba53d41f42587
Michael Knudsen <michaelk@clin.au.dk>
2017-01-04 14:12:14 +0100

    Added unit tests for Interval class

--- tests/test_plotxhmm.py
+++ tests/test_plotxhmm.py
@@ -4,4 +4,14 @@ from plotxhmm.plotxhmm import Interval
 class TestPlotXHMM(unittest.TestCase):
 
     def test_interval(self):
-        pass
+
+        self.assertFalse(Interval('chr1', 1000, 2000).overlaps(Interval('chr2', 1000, 2000)))
+
+        self.assertFalse(Interval('chr1', 1000, 2000).overlaps(Interval('chr1', 3000, 4000)))
+        self.assertFalse(Interval('chr1', 1000, 2000).overlaps(Interval('chr1', 500, 999)))
+        self.assertFalse(Interval('chr1', 1000, 2000).overlaps(Interval('chr1', 2001, 3000)))
+
+        self.assertTrue(Interval('chr1', 1000, 2000).overlaps(Interval('chr1', 500, 1500)))
+        self.assertTrue(Interval('chr1', 1000, 2000).overlaps(Interval('chr1', 1500, 2500)))
+        self.assertTrue(Interval('chr1', 1000, 2000).overlaps(Interval('chr1', 500, 1000)))
+        self.assertTrue(Interval('chr1', 1000, 2000).overlaps(Interval('chr1', 2000, 2500)))
